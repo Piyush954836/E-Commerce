32f689ee3370d33042376610a331d8d097fa73c4
Piyush954836 <panwarpiyush653@gmail.com>
2025-06-23 14:58:22 +0530

    Registration page added

---
+++ client/js/register.js
@@ -0,0 +1,32 @@
+document.querySelector('form').addEventListener('submit', async function(e){
+  e.preventDefault();
+
+  const name = document.getElementById('fullName').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try{
+    const response = fetch('/api/users/register', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      })
+    });
+
+    const data = await response.json();
+    if(response.ok){
+      alert('Registration Successfull');
+    }else{
+      alert('Error: '+ (data.message || "Registration failed"));
+    }
+  }
+  catch(error){
+    console.error("Error: ", error);
+    alert("Something went wrong. Please try again");
+  }
+});
